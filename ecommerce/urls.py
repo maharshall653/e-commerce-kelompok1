fb74ab0e430a37331f7c7acc62f3961a816c8323
maharshall <maharshall653@gmail.com>
2022-11-26 19:10:14 +0700

    e-commerce tiga

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,7 +1,7 @@
 """ecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
+    path('' , include('home.urls') ),
+    path('product/' , include('products.urls') ),
+    path('accounts/' , include('accounts.urls') ),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
